f4a3d1af5c832894944b640beeefa0a57e7a0535
Ervis Trupja <ervistrupja@outlook.com>
2016-12-30 23:29:23 +0100

    All Angular2 Materialize libraries are added to the project

--- app/app.module.ts
+++ app/app.module.ts
@@ -18,6 +18,22 @@ import {ContractsComponent} from './contracts/contracts.component'
 import {MdCardModule} from '@angular2-material/card'
 import {MdButtonModule} from '@angular2-material/button'
 import {MdIconModule} from '@angular2-material/icon'
+import {MdMenuModule} from '@angular2-material/menu'
+import {MdButtonToggleModule} from '@angular2-material/button-toggle'
+import {MdCheckboxModule} from '@angular2-material/checkbox'
+import {MdGridListModule} from '@angular2-material/grid-list';
+import {MdInputModule} from '@angular2-material/input';
+import {MdListModule} from '@angular2-material/list';
+import {MdProgressBarModule} from '@angular2-material/progress-bar';
+import {MdProgressCircleModule} from '@angular2-material/progress-circle';
+import {MdRadioModule} from '@angular2-material/radio';
+import {MdSidenavModule} from '@angular2-material/sidenav';
+import {MdSliderModule} from '@angular2-material/slider';
+import {MdSlideToggleModule} from '@angular2-material/slide-toggle';
+import {MdTabsModule} from '@angular2-material/tabs';
+import {MdToolbarModule} from '@angular2-material/toolbar';
+import {MdTooltipModule} from '@angular2-material/tooltip';
+
 
 import {MdIconRegistry} from '@angular2-material/icon'
 
@@ -26,7 +42,9 @@ import {routing} from './app.routes';
 
 @NgModule({
   imports:      [ 
-    BrowserModule, MdCardModule, MdButtonModule, MdIconModule,
+    BrowserModule, 
+    MdCardModule, MdButtonModule, MdIconModule,MdMenuModule,MdButtonToggleModule,MdCheckboxModule,MdGridListModule,MdInputModule,MdListModule,MdProgressBarModule,
+    MdProgressCircleModule,MdRadioModule,MdSidenavModule,MdSliderModule,MdSlideToggleModule,MdTabsModule,MdToolbarModule,MdTooltipModule,
     FormsModule,
     HttpModule,
     routing
--- systemjs.config.js
+++ systemjs.config.js
@@ -26,10 +26,28 @@
       // other libraries
       'rxjs':                      'npm:rxjs',
       'angular-in-memory-web-api': 'npm:angular-in-memory-web-api/bundles/in-memory-web-api.umd.js',
+
+      //Angular2 Material libraries (all available from npmjs website )
       '@angular2-material/core':'npm:@angular2-material/core/core.umd.js',
       '@angular2-material/card':'npm:@angular2-material/card/card.umd.js',
       '@angular2-material/button':'npm:@angular2-material/button/button.umd.js',
       '@angular2-material/icon':'npm:@angular2-material/icon/icon.umd.js',
+      '@angular2-material/menu':'npm:@angular2-material/menu/menu.umd.js',
+      '@angular2-material/button-toggle':'npm:@angular2-material/button-toggle/button-toggle.umd.js',
+      '@angular2-material/checkbox':'npm:@angular2-material/checkbox/checkbox.umd.js',
+      '@angular2-material/grid-list':'npm:@angular2-material/grid-list/grid-list.umd.js',
+      '@angular2-material/input':'npm:@angular2-material/input/input.umd.js',
+      '@angular2-material/list':'npm:@angular2-material/list/list.umd.js',
+      '@angular2-material/progress-bar':'npm:@angular2-material/progress-bar/progress-bar.umd.js',
+      '@angular2-material/progress-circle':'npm:@angular2-material/progress-circle/progress-circle.umd.js',
+      '@angular2-material/radio':'npm:@angular2-material/radio/radio.umd.js',
+      '@angular2-material/sidenav':'npm:@angular2-material/sidenav/sidenav.umd.js',
+      '@angular2-material/slider':'npm:@angular2-material/slider/slider.umd.js',
+      '@angular2-material/slide-toggle':'npm:@angular2-material/slide-toggle/slide-toggle.umd.js',
+      '@angular2-material/tabs':'npm:@angular2-material/tabs/tabs.umd.js',
+      '@angular2-material/toolbar':'npm:@angular2-material/toolbar/toolbar.umd.js',
+      '@angular2-material/tooltip':'npm:@angular2-material/tooltip/tooltip.umd.js',
+      
     },
     // packages tells the System loader how to load when no filename and/or no extension
     packages: {
